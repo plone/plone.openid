5be20916c5d7b16487680725be5f7bd7f0107961
Wichert Akkerman <wichert@wiggy.net>
2006-10-03 08:29:52 +0000

    Add missing bits
    svn path=/plone.openid/trunk/; revision=11040

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='plone.openid',
+      version=version,
+      description="OpenID authentication support for PAS",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Zope2",
+	"License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+	"Topic :: System :: Systems Administration :: Authentication/Directory",
+        ],
+      keywords='PAS openid',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/plone/plone.openid',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
