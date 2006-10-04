59c8d12a47e7e6fd008ea2e24b62f9672a446f5d
Wichert Akkerman <wichert@wiggy.net>
2006-10-04 11:13:47 +0000

    Perform field valdation in the complete method
    svn path=/plone.openid/trunk/; revision=11052

--- plone/openid/tests/consumer.py
+++ plone/openid/tests/consumer.py
@@ -1,5 +1,5 @@
 
-from openid.consumer.consumer import SUCCESS
+from openid.consumer.consumer import FAILURE, SUCCESS
 
 class MockAuthRequest:
     """Amock OpenID AuthRequest.
@@ -17,7 +17,17 @@ class MockConsumer:
         return MockAuthRequest()
 
     def complete(self, credentials):
-        result=MockAuthRequest(status=SUCCESS,
+        status=SUCCESS
+        for field in [ "openid.source", "nonce", "openid.identity",
+                "openid.assoc_handle", "openid.return_to", "openid.signed",
+                "openid.sig", "openid.invalidate_handle", "openid.mode"]:
+            if field not in credentials:
+                status=FAILURE
+
+        if credentials["openid.identity"]!=self.identity:
+            status=FAILURE
+
+        result=MockAuthRequest(status=status,
                                 identity_url=self.identity)
 
 def PatchPlugin(plugin):
