91c88fd3a01a41c8e81d974fd698415b85b100c0
Wichert Akkerman <wichert@wiggy.net>
2007-01-02 22:52:07 +0000

    Add a check to see if the OpenID packages are available
    svn path=/plone.openid/trunk/; revision=11862

--- plone/openid/__init__.py
+++ plone/openid/__init__.py
@@ -1,16 +1,24 @@
 from AccessControl.Permissions import manage_users as ManageUsers
 from Products.PluggableAuthService.PluggableAuthService import registerMultiPlugin
 
-from plugins import oid
+from plone.openid.config import HAS_OPENID
 
-registerMultiPlugin(oid.OpenIdPlugin.meta_type)
+if HAS_OPENID:
+    from plugins import oid
+    registerMultiPlugin(oid.OpenIdPlugin.meta_type)
+else:
+    import logging
+    logger=logging.getLogger("Plone")
+    logger.info("OpenID packages not installed")
+
 
 def initialize(context):
-    context.registerClass(oid.OpenIdPlugin,
-                            permission=ManageUsers,
-                            constructors=
-                                    (oid.manage_addOpenIdPlugin,
-                                    oid.addOpenIdPlugin),
-                            visibility=None,
-                            icon="www/openid.png")
+    if HAS_OPENID:
+        context.registerClass(oid.OpenIdPlugin,
+                                permission=ManageUsers,
+                                constructors=
+                                        (oid.manage_addOpenIdPlugin,
+                                        oid.addOpenIdPlugin),
+                                visibility=None,
+                                icon="www/openid.png")
 
---
+++ plone/openid/config.py
@@ -0,0 +1,7 @@
+try:
+    import openid
+    import urljr
+    HAS_OPENID=True
+except ImportError:
+    HAS_OPENID=False
+
