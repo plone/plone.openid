2e2d9195c646686f47a477287e9938117eabdb9c
Maurits van Rees <maurits@vanrees.org>
2016-02-11 21:46:07 +0100

    txt to rst

--- setup.py
+++ setup.py
@@ -2,32 +2,33 @@ from setuptools import setup, find_packages
 
 version = '2.0.5.dev0'
 
-setup(name='plone.openid',
-      version=version,
-      description="OpenID authentication support for PAS",
-      long_description=open("README.txt").read() + '\n' +
-                       open("CHANGES.txt").read(),
-      classifiers=[
-          "Environment :: Web Environment",
-          "Framework :: Plone",
-          "Framework :: Plone :: 5.0",
-          "Framework :: Zope2",
-          "License :: OSI Approved :: BSD License",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 2.7",
-          "Topic :: System :: Systems Administration :: Authentication/Directory",
-        ],
-      keywords='PAS openid authentication',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='https://github.com/plone/plone.openid',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
+setup(
+    name='plone.openid',
+    version=version,
+    description="OpenID authentication support for PAS",
+    long_description=(open("README.rst").read() + '\n' +
+                      open("CHANGES.rst").read()),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: System :: Systems Administration :: Authentication/Directory",  # noqa
+    ],
+    keywords='PAS openid authentication',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://github.com/plone/plone.openid',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
         'setuptools',
         'python-openid >=2.2.1,<2.3dev',
         'transaction',
@@ -35,5 +36,5 @@ setup(name='plone.openid',
         'Products.PluggableAuthService',
         'ZODB3',
         'Zope2',
-      ],
-      )
+    ],
+    )
