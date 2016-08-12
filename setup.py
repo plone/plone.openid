83375fa8594e9fb23ca2d4e1959c5d4a0a6cf63b
Maurits van Rees <maurits@vanrees.org>
2016-08-12 22:48:17 +0200

    Added Plone 5.1 classifier to setup.py.
        [ci skip]

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
         "Framework :: Zope2",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
